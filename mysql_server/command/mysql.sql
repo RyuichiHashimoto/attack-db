7e263d7062579bcbe6b79874121962470b49b129
hashimoto <ryuichi.hashimoto.git@gmail.com>
2024-06-24 00:37:29 +0900

    []ada

--- mysql_server/command/mysql.sql
+++ mysql_server/command/mysql.sql
@@ -1,44 +1,45 @@
-#  mysql --skip-ssl -h miter-attack-db  -proot miter-attack  --batch --silent  < mysql
-# SELECT 'technique_id', 'technique_name', 'tactic_name', 'datasource_id', 'datasource', 'collection_layer', 'URL'
-# UNION ALL
-SELECT 
-    t.technique_id,
-    t.technique AS technique_name,
-    tc.tactic AS tactic_name,
-    ds.datasource_id AS datasource_id,
-    ds.datasource AS datasource,
+SELECT
+    ck.phase AS killchain,
+    tc.tactic AS tactic,
+    t.technique AS technique,
     c.collection_layer AS collection_layer,
-    ck.phase AS phase,
+    ds.datasource AS datasource,
     t.url
 FROM
     techniques t
-LEFT JOIN
-    tactech tt ON t.technique_id = tt.technique_id
-LEFT JOIN
-    tactics tc ON tt.tactic_id = tc.tactic_id
-LEFT JOIN
-    techdata td ON t.technique_id = td.technique_id
-LEFT JOIN
-    datasources ds ON td.datasource_id = ds.datasource_id
-LEFT JOIN
-    datacollection c ON ds.datasource_id = c.datasource_id
-LEFT JOIN
-    cyberkillchain ck ON tc.killchain_id = ck.killchain_id
-ORDER BY 
-    t.sequence;
+    LEFT JOIN tactech tt ON t.technique_id = tt.technique_id
+    LEFT JOIN tactics tc ON tt.tactic_id = tc.tactic_id
+    LEFT JOIN techdata td ON t.technique_id = td.technique_id
+    LEFT JOIN datasources ds ON td.datasource_id = ds.datasource_id
+    LEFT JOIN datacollection c ON ds.datasource_id = c.datasource_id
+    LEFT JOIN cyberkillchain ck ON tc.killchain_id = ck.killchain_id
 WHERE
     "1" = "1"
     # c.collection_layer = "HOST"
     # c.collection_layer = "Network"
-    AND c.collection_layer in ("HOST", "Network","OSINT")
-
-    AND tc.tactic in ("Reconnaissance", "Resource Development")
-    # AND tc.tactic in ("Initial Access", "Execution", "Persistence", "Privilege Escalation", "Defense Evasion", "Credential Access", "Discovery", "Lateral Movement", "Collection")
+    AND c.collection_layer in ("HOST", "Network", "OSINT")
+    -- AND tc.tactic in (
+    --     "Reconnaissance",
+    --     "Resource Development",
+    --     "Initial Access"
+    -- )
+    AND tc.tactic in (
+        "Initial Access",
+        "Execution",
+        "Persistence",
+        "Privilege Escalation",
+        "Defense Evasion",
+        "Credential Access",
+        "Discovery",
+        "Lateral Movement",
+        "Collection"
+    )
     # AND tc.tactic in ("Exfiltration", "Command and Control", "Impact")
-AND t.is_sub_technique = false
+    AND t.is_sub_technique = false
+ORDER BY tc.sequence;
 # INTO
 #     # OUTFILE '/home/work/mysql_server/command/output1.csv'
 #     OUTFILE './1_intrusion.csv'
-#     FIELDS TERMINATED BY ',' 
+#     FIELDS TERMINATED BY ','
 #     ENCLOSED BY '"'
 #     LINES TERMINATED BY '\n';
--- src/miter/create_pivot.py
+++ src/miter/create_pivot.py
@@ -2,31 +2,34 @@ import pandas as pd
 import mysql.connector
 from db.db import parameter
 
+def load_sql_file(file) -> str:
+    with open(file, "r") as fin:
+        return fin.read()
+
+
+
 if __name__ == "__main__":
+    ret = load_sql_file("/home/work/mysql_server/command/mysql.sql")
+    print(ret)
 
 
     # MySQLデータベースに接続
     db_connection = mysql.connector.connect(**parameter, ssl_disabled=True)
 
-    # SQLクエリを実行してデータを取得
-    query = '''
-    SELECT datasource, technique, COUNT(*) as combination_count
-    FROM techdata td
-    LEFT JOIN datasources ds ON ds.datasource_id = td.datasource_id
-    LEFT JOIN techniques t ON t.technique_id = td.technique_id
-    GROUP BY td.datasource_id, td.technique_id
-    '''
+    # # SQLクエリを実行してデータを取得
+    query = load_sql_file("/home/work/mysql_server/command/mysql.sql")    
     df = pd.read_sql(query, db_connection)
 
-    # ピボットテーブルを作成
+    # # ピボットテーブルを作成
     pivot_table = df.pivot_table(
-        index='datasource',
-        columns='technique',
+        index=['collection_layer', 'datasource'],
+        columns=["tactic", 'technique'],
         values='combination_count',
+        aggfunc='max',
         fill_value=0
     )
-    # 必要に応じて、CSVなどのファイルに保存
-    pivot_table.to_csv('pivot_table.csv')
+    # # 必要に応じて、CSVなどのファイルに保存
+    pivot_table.to_excel("to_excel.xlsx")
 
-    # データベース接続を閉じる
+    # # データベース接続を閉じる
     db_connection.close()
